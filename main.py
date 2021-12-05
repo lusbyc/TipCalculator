dc6861afda306224028ad99f68f3fb64b52fe8a8
lusbyc <43541354+lusbyc@users.noreply.github.com>
2021-12-05 10:19:47 -0500

    changed camel case to snake case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Exercise 3: Tip Calculator")
 
-totalBill = float(input("What's the total amount of the bill? "))
+total_bill = float(input("What's the total amount of the bill? "))
 num_of_people = int(input("How many people were there? "))
-total_per_person = format(totalBill / num_of_people,".2f")
+total_per_person = format(total_bill / num_of_people,".2f")
 print(f"The total per person is ${total_per_person}.")
